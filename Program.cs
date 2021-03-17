f872318b02d2348ec987f7822421458109ba49d7
Kerim ALTUN <76987626+zigetvar@users.noreply.github.com>
2021-03-17 05:40:02 +0300

    C#'la Ogrenci Uygulamasi

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ögernciUygulaması
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool kontrol = true;
+            Student ögrenci1 = new Student("Kerim","ALTUN",2011404028,80,65,70,"Mehmet Akif Ersoy Üniversitesi");
+
+            Console.WriteLine("Öğrenci Uygulamamıza Hoşgeldiniz... Lütfen Yapmak İstediğiniz İşlemi Seçiniz...");
+
+            while (kontrol)
+            {
+                islemler();
+                string secim = Console.ReadLine();
+
+                switch (secim)
+                {
+                    case "1":
+
+                        ögrenci1.ÖgrenciBilgileriGöster();
+                        break;
+
+                    case "2":
+                        ögrenci1.ÖgrenciOrtalamasınıBul();
+                        break;
+
+                    case "3":
+                        ögrenci1.OkulGetir();
+                        break;
+
+                    case "4":
+                        kontrol = false;
+                        break;
+
+
+                    
+                }
+            }
+
+
+            
+            
+            
+            static void islemler()
+            {
+                Console.WriteLine("1-Öğrenci Bilgilerini Göster");
+                Console.WriteLine("2-Öğrenci Ortalaması Göster");
+                Console.WriteLine("3-Öğrenci Okulu Göster");
+                Console.WriteLine("4-Çıkış");
+            }
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ögernciUygulaması
+{
+    public class Student
+    {
+        private string isim;
+        private string soyisim;
+        private int ögrno;
+        private int vize1;
+        private int vize2;
+        private int final;
+        private string okul;
+
+
+        public Student(string _isim, string _soyisim, int _ögrno, int _vize1, int _vize2, int _final, string _okul)
+        {
+            isim = _isim;
+            soyisim = _soyisim;
+            ögrno = _ögrno;
+            vize1 = _vize1;
+            vize2 = _vize2;
+            final = _final;
+            okul = _okul;
+        }
+
+
+        public void ÖgrenciBilgileriGöster()
+        {
+            Console.WriteLine("Öğrenci ismi:"+ isim);
+            Console.WriteLine("Öğrenci soyismi:" + soyisim);
+            Console.WriteLine("Öğrenci no:" + ögrno);
+            Console.WriteLine("Öğrenci vize1:" + vize1);
+            Console.WriteLine("Öğrenci vize2:" + vize2);
+            Console.WriteLine("Öğrenci final:" + final);
+            Console.WriteLine("Öğrenci okulu:" + okul);
+        }
+
+        public double ÖgrenciOrtalamasınıBul()
+        {
+            double ortalama = vize1 * 0.2 + vize2 * 0.2 + final * 0.6;
+            Console.WriteLine("Öğrenci Ortalaması:"+ ortalama);
+            return ortalama;
+        }
+        public void OkulGetir()
+        {
+            Console.WriteLine("Öğrenci okulu:"+ okul);
+        }
+
+        
+    }
+}
